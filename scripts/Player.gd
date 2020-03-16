4580dfa7edd7dc1f6f1c30f1bfa1c89c9ca883a3
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-03-16 14:07:12 +0100

    Add player movement and jumping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,24 @@
 extends RigidBody2D
 
-export var speed = 128
-
-func _unhandled_input(event):
-	var axis = event.get_action_strength("right") - event.get_action_strength("left")
-	.set_axis_velocity(Vector2(axis * speed, 0))
+export var speed = 512
+export var jumpForce = 1024
+export var jumpCount = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var lv = .get_linear_velocity()
+	
+	var axis = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var shouldJump = jumpCount > 0 && Input.is_action_just_pressed("jump")
+	var appliedY = -jumpForce if shouldJump else lv.y
+	var appliedX = axis * speed
+	var applied = Vector2(appliedX, appliedY)
+	
+	.set_linear_velocity(applied)
+	
+	if shouldJump:
+		jumpCount -= 1;
